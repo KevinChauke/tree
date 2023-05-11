c46542dcab93a5b6da33b808d3a40d14903f6396
KevinChauke <s.chauke@lab.clicklearning.org>
2023-05-11 10:24:15 +0200

    J.s-Tree of stars

---
+++ tree.js
@@ -0,0 +1,24 @@
+let n = 5;
+let stars = "";
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    stars += "*";
+  }
+  stars += "\n";
+}
+console.log(stars);
+
+
+/* 
+   -To create the tree i had to use 2 for loops, 
+    1 external that will take care of the columns 
+    and 1 internal that will print rows of the stars.
+
+   -I had to declare a variable "n" that will take the 
+    number of times the program should run. The internal 
+    loop will run according to the external loop. 
+   
+   -The internal loop will run 1 time in the 1st execution 
+    of the external loop until, then adds 1 during the 2nd 
+    execution untill it reaches "n" times.
+*/
